d57bf7424b32dcf7b509b3f1f2d5ddcef0f620a3
Rick Vodicka <rick@inspecdigital.org>
2015-01-24 12:48:03 -0800

    background css changes

--- wp-content/themes/boxmart/page.php
+++ wp-content/themes/boxmart/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <div class="container">
-    <div id="main" class="clearfix" role="main">
+    <div id="main" class="clearfix bgcolorsecondary" role="main">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
         <?php the_content(); ?>
